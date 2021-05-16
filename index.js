10f71ee9719bdf2a46eb238aeb2dc9c77073dccd
Joseflorezv07 <joseflorezv07@gmial.com>
2021-05-15 21:33:34 -0500

    new changes

--- index.js
+++
@@ -1,6 +0,0 @@
-const navToogle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-
-navToogle.addEventListener("click",()=>{
-    navMenu.classList.toggle("nav-menu_visible");
-});
